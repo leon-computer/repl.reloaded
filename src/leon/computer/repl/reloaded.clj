c4c808e259e056a3829380c98b33996accfefaeb
Leon Grapenthin <mail@leon.computer>
2024-01-07 12:05:15 +0100

    report start/stop

--- src/leon/computer/repl/reloaded.clj
+++ src/leon/computer/repl/reloaded.clj
@@ -24,6 +24,45 @@
                   (constantly
                    (into new-unloaded-system system))))
 
+(defn wrap-report [system]
+  (reduce-kv (fn [system k c-unwrapped]
+               (cond-> system
+                 (not= k ::status)
+                 (assoc k
+                        (-> c-unwrapped
+                            (vary-meta
+                             assoc
+                             `component/start
+                             (fn [c-wrapped]
+                               (let [_ (print k)
+                                     _ (flush)
+                                     c-started
+                                     (-> c-wrapped
+                                         (vary-meta dissoc `component/start)
+                                         ;; restore original start-fn if present
+                                         (vary-meta
+                                          merge
+                                          (find c-unwrapped
+                                                `component/start))
+                                         (component/start))
+                                     _ (println " ✓")]
+                                 (-> c-started
+                                     (vary-meta
+                                      assoc
+                                      `component/stop
+                                      (fn [c-wrapped]
+                                        (let [_ (print k)
+                                              _ (flush)
+                                              c-stopped
+                                              (-> c-wrapped
+                                                  (vary-meta dissoc `component/stop)
+                                                  (vary-meta merge
+                                                             (find c-started `component/stop))
+                                                  (component/stop))
+                                              _ (println " ✓")]
+                                          c-stopped)))))))))))
+             system system))
+
 (defn stop []
   (let [{{:keys [started? stopped?]} ::status} system
         system
@@ -51,7 +90,7 @@
                 (println
                  (str "[SYSTEM] Warning: No system set. "
                       "(Use set-system-to-load!)")))
-              (component/start-system unloaded-system)))]
+              (component/start-system (wrap-report unloaded-system))))]
     (alter-var-root #'system (constantly system))))
 
 (defn reset []
